8e4cde8752f2b5e294b64272803545b1c2dba42f
macooo3 <marco.ysu765@gmail.com>
2024-11-14 01:34:00 -0500

    initial

---
+++ script.js
@@ -0,0 +1,10 @@
+'strict'
+const 
+
+
+const calculate = function (a, b, operator) {
+    if (operator === '+') return a + b;
+    if (operator === '-') return a - b;
+    if (operator === '/') return a / b;
+    if (operator === '*') return a * b;
+}
